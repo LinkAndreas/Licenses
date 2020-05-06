b6448466043afd11aa59e24681dd8419a89d735b
Andreas Link <mail@linkandreas.de>
2020-05-06 17:38:32 +0200

    Perform initial xcode project setup

---
+++ App/Generated/SwiftGen/Images.swift
@@ -0,0 +1,16 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+import UIKit.UIImage
+
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable file_length
+
+// swiftlint:disable identifier_name line_length nesting type_body_length type_name
+internal enum Images {
+  private static let bundle = Bundle(for: BundleToken.self)
+}
+// swiftlint:enable identifier_name line_length nesting type_body_length type_name
+
+// MARK: - Implementation Details
+private final class BundleToken {}
