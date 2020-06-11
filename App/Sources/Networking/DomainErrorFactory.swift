0fdd46b3ed26d93ce9e41785aea19654fe4be8a4
Andreas Link <mail@linkandreas.de>
2020-06-11 13:43:08 +0200

    Upgrade aphrodite

--- App/Sources/Networking/DomainErrorFactory.swift
+++ App/Sources/Networking/DomainErrorFactory.swift
@@ -4,51 +4,23 @@ import Aphrodite
 import Foundation
 
 enum DomainError: Error {
-    case encoding(EncodingError)
-    case decoding(DecodingError)
-    case client(HTTPURLResponse, Int)
-    case server(HTTPURLResponse, Int)
-    case unauthorized(HTTPURLResponse)
-    case underlying(HTTPURLResponse, Error)
-    case forbidden(HTTPURLResponse)
-    case notFound(HTTPURLResponse)
-    case notConnectedToInternet
+    case forbidden(HTTPURLResponse, Data)
     case serviceCancelled
+    case notConnectedToInternet
     case unexpected
 }
 
 enum DomainErrorFactory: AphroditeDomainErrorFactory {
-    static func make(from error: AphroditeError) -> DomainError { // swiftlint:disable:this cyclomatic_complexity
+    static func make(from error: AphroditeError) -> DomainError {
         switch error {
-        case let .underlying(response, error):
-            return .underlying(response, error)
-
-        case let .unauthorized(response):
-            return .unauthorized(response)
-
-        case let .forbidden(response):
-            return .forbidden(response)
-
-        case let .notFound(response):
-            return .notFound(response)
-
-        case let .encoding(error):
-            return .encoding(error)
-
-        case let .decoding(error):
-            return .decoding(error)
-
-        case let .client(response, statusCode):
-            return .client(response, statusCode)
-
-        case let .server(response, statusCode):
-            return .server(response, statusCode)
-
-        case .notConnectedToInternet:
-            return .notConnectedToInternet
+        case let .forbidden(response, data):
+            return .forbidden(response, data)
 
         case .serviceCancelled:
             return .serviceCancelled
+
+        case .notConnectedToInternet:
+            return .notConnectedToInternet
 
         default:
             return .unexpected
