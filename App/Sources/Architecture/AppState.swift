5f5642fdcb77810301c5eaa79f820f959b7c99e9
Andreas Link <mail@linkandreas.de>
2020-06-17 20:17:54 +0200

    Show activity indicator if needed

--- App/Sources/Architecture/AppState.swift
+++ App/Sources/Architecture/AppState.swift
@@ -1,12 +1,25 @@
 //  Copyright © 2020 Andreas Link. All rights reserved.
 
+import Foundation
+
 struct AppState: Equatable {
+    var isTargeted: Bool
     var repositories: [GithubRepository]
     var selectedRepository: GithubRepository?
-    var isTargeted: Bool
+    var githubRequestStatus: GithubRequestStatus?
+    var processingUUIDs: Set<UUID>
 
-    init(repositories: [GithubRepository] = [], isTargeted: Bool = false) {
-        self.repositories = repositories
+    init(
+        isTargeted: Bool = false,
+        repositories: [GithubRepository] = [],
+        selectedRepository: GithubRepository? = nil,
+        githubRequestStatus: GithubRequestStatus? = nil,
+        processingUUIDs: Set<UUID> = []
+    ) {
         self.isTargeted = isTargeted
+        self.repositories = repositories
+        self.selectedRepository = selectedRepository
+        self.githubRequestStatus = githubRequestStatus
+        self.processingUUIDs = processingUUIDs
     }
 }
