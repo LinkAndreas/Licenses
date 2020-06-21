6d688b1573d3a8fb20e41a808a5e25451914afc9
Andreas Link <mail@linkandreas.de>
2020-06-21 13:39:34 +0200

    Introduce RequestLimitView and include previews

--- App/Sources/UI/RepositoryDetail.swift
+++ App/Sources/UI/RepositoryDetail.swift
@@ -32,7 +32,59 @@ struct RepositoryDetail: View {
                         Text("Please select a repository...")
                     }
                 }.padding()
-            }.frame(width: geometry.size.width, height: geometry.size.width)
+            }.frame(width: geometry.size.width, height: geometry.size.height)
         }
+    }
+}
+
+struct RepositoryDetail_Previews: PreviewProvider {
+    static let decodedContent: String = """
+        The MIT License (MIT)
+
+        Copyright (c) 2019 XMARTLABS
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.
+    """
+
+    static var previews: some View {
+        RepositoryDetail()
+            .environmentObject(
+                Store(
+                    initialState: AppState(
+                        isTargeted: false,
+                        repositories: [
+                            GithubRepository(
+                                packageManager: .carthage,
+                                name: "Eureka",
+                                version: "5.2.1",
+                                author: "xmartlabs",
+                                url: URL(string: "https://github.com/xmartlabs/Eureka"),
+                                license: .init(decodedContent: decodedContent)
+                            )
+                        ],
+                        githubRequestStatus: nil,
+                        progress: 0.5
+                    ),
+                    reducer: ReducerFactory.appReducer,
+                    environment: AppEnvironment()
+            )
+        )
+        .previewLayout(.fixed(width: 650, height: 500))
     }
 }
--- App/Sources/UI/RespositoryList.swift
+++ App/Sources/UI/RespositoryList.swift
@@ -23,10 +23,44 @@ struct RepositoryList: View {
                         }
                         .padding()
                         .tag(repository)
+                        .animation(nil)
                     }
+                }
+                .animation(.linear)
+                if viewStore.progress != nil {
+                    ProgressBar(value: .constant(viewStore.progress!))
+                        .frame(height: 5)
+                        .padding([.leading, .trailing, .bottom], 8)
                 }
                 GithubRequestLimitView()
             }
         }
+    }
+}
+
+struct RespositoryList_Previews: PreviewProvider {
+    static var previews: some View {
+        RepositoryList()
+            .environmentObject(
+                Store(
+                    initialState: AppState(
+                        isTargeted: false,
+                        repositories: [
+                            GithubRepository(
+                                packageManager: .carthage,
+                                name: "Eureka",
+                                version: "5.2.1",
+                                author: "xmartlabs",
+                                url: URL(string: "https://github.com/xmartlabs/Eureka")
+                            )
+                        ],
+                        githubRequestStatus: nil,
+                        progress: 0.5
+                    ),
+                    reducer: ReducerFactory.appReducer,
+                    environment: AppEnvironment()
+                )
+        )
+        .previewLayout(.fixed(width: 350, height: 500))
     }
 }
