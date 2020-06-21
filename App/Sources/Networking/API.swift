5fd73f7a2ea4d814364a752f1c90dd743dc8c143
Andreas Link <mail@linkandreas.de>
2020-06-21 13:40:38 +0200

    Include AuthPlugin

--- App/Sources/Networking/API.swift
+++ App/Sources/Networking/API.swift
@@ -13,6 +13,7 @@ let API: Aphrodite<DomainErrorFactory> = .init(
             NetworkLoggerPlugin()
         ],
         .github: [
+            AuthPlugin(),
             RemainingGithubRequestsPlugin()
         ]
     ]
