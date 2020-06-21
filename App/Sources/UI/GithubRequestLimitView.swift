4d9b2bcb5a28cafc06c2c3374e1449418fb46994
Andreas Link <mail@linkandreas.de>
2020-06-21 13:39:34 +0200

    Introduce RequestLimitView and include previews

--- App/Sources/UI/GithubRequestLimitView.swift
+++ App/Sources/UI/GithubRequestLimitView.swift
@@ -7,12 +7,36 @@ struct GithubRequestLimitView: View {
 
     var body: some View {
         ViewStoreProvider(self.store) { viewStore in
-            if viewStore.githubRequestStatus != nil && viewStore.githubRequestStatus!.remaining == 0 {
-                Text("Github Request limit exceeded. Please add a personal access token in settings.")
-                    .foregroundColor(.red)
-                    .padding([.top], 2)
-                    .padding([.leading, .bottom, .trailing], 8)
+            if viewStore.githubRequestStatus != nil {
+                Group {
+                    if viewStore.githubRequestStatus!.remaining == 0 {
+                        Text("Github Request limit exceeded. Please add a personal access token in settings.")
+                            .foregroundColor(.red)
+                    }
+                }
+                .padding([.top], 2)
+                .padding([.leading, .bottom, .trailing], 8)
             }
         }
+    }
+}
+
+struct GithubRequestLimitView_Previews: PreviewProvider {
+    static var previews: some View {
+        GithubRequestLimitView()
+            .environmentObject(
+                Store(
+                    initialState: AppState(
+                        isTargeted: false,
+                        repositories: [],
+                        selectedRepository: nil,
+                        githubRequestStatus: .init(limit: 40, remaining: 0, resetInterval: 30),
+                        progress: 0.5
+                    ),
+                    reducer: ReducerFactory.appReducer,
+                    environment: AppEnvironment()
+            )
+        )
+        .previewLayout(.fixed(width: 650, height: 500))
     }
 }
