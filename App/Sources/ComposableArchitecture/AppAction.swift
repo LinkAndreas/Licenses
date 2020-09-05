0801b5da560e44e420e52550c5da4ddf29f4e71e
Andreas Link <mail@linkandreas.de>
2020-09-05 07:45:57 +0200

    Implement AppAction

---
+++ App/Sources/ComposableArchitecture/AppAction.swift
@@ -0,0 +1,20 @@
+//  Copyright © 2020 Andreas Link. All rights reserved.
+
+import Foundation
+
+enum AppAction: Equatable {
+    case resetProgress
+    case updateProgress
+    case didStartFetchingLicenses
+    case didStopFetchingLicenses
+    case didProcess(repository: GithubRepository)
+    case selectedRepository(id: UUID?)
+    case startSearchingManifests
+    case stopSearchingManifests
+    case searchManifests(filePaths: [URL])
+    case add(repository: GithubRepository)
+    case handle(providers: [NSItemProvider])
+    case fetchLicenses
+    case exportLicenses(destination: URL)
+    case update(isTargeted: Bool)
+}
