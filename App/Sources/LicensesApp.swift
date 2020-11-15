bb1a897d9fcb10d14b53fe4d2f386baa0ee559c3
Andreas Link <mail@linkandreas.de>
2020-11-15 20:22:10 +0100

    Migrate to SwiftUI Lifecycle

---
+++ App/Sources/LicensesApp.swift
@@ -0,0 +1,30 @@
+//  Copyright © 2020 Andreas Link. All rights reserved.
+
+import AppKit
+import ComposableArchitecture
+import SwiftUI
+
+@main
+struct LicensesApp: App {
+    // swiftlint:disable:next weak_delegate
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate: AppDelegate
+
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+                .frame(
+                    minWidth: 650,
+                    idealWidth: 800,
+                    maxWidth: .infinity,
+                    minHeight: 450,
+                    idealHeight: 600,
+                    maxHeight: .infinity,
+                    alignment: .center
+                )
+        }
+
+        Settings {
+            SettingsView()
+        }
+    }
+}
