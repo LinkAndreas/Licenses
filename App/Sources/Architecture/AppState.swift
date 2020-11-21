81d564178e9130162a8b32201ff363f2e5d07eb7
Andreas Link <mail@linkandreas.de>
2020-11-21 15:06:24 +0100

    Introduce store empty state

--- App/Sources/Architecture/AppState.swift
+++ App/Sources/Architecture/AppState.swift
@@ -1,6 +1,8 @@
 //  Copyright © 2020 Andreas Link. All rights reserved.
 
 struct AppState: Equatable {
+    static let empty: Self = .init()
+
     var isProcessing: Bool
     var isTargeted: Bool
     var progress: Float?
@@ -9,6 +11,26 @@ struct AppState: Equatable {
     var errorMessage: String?
     var selectedRepository: GithubRepository?
     var repositories: [GithubRepository]
+
+    init(
+        isProcessing: Bool = false,
+        isTargeted: Bool = false,
+        progress: Float? = nil,
+        remainingRepositories: Int = 0,
+        totalRepositories: Int = 0,
+        errorMessage: String? = nil,
+        selectedRepository: GithubRepository? = nil,
+        repositories: [GithubRepository] = []
+    ) {
+        self.isProcessing = isProcessing
+        self.isTargeted = isTargeted
+        self.progress = progress
+        self.remainingRepositories = remainingRepositories
+        self.totalRepositories = totalRepositories
+        self.errorMessage = errorMessage
+        self.selectedRepository = selectedRepository
+        self.repositories = repositories
+    }
 }
 
 extension AppState {
--- App/Sources/UI/MainView.swift
+++ App/Sources/UI/MainView.swift
@@ -4,53 +4,51 @@ import SwiftUI
 
 struct MainView: View {
     @StateObject private var store: Store<AppState, AppAction, AppEnvironment> = .init(
-        initialState: .init(
-            isProcessing: false,
-            isTargeted: false,
-            progress: nil,
-            remainingRepositories: 0,
-            totalRepositories: 0,
-            errorMessage: nil,
-            selectedRepository: nil,
-            repositories: []
-        ),
+        initialState: .empty,
         reducer: appReducer,
         environment: .init()
     )
 
     var body: some View {
         FileDropArea {
-            SplitView()
-                .toolbar {
-                    ToolbarItems(
-                        openFiles: {
-                            let openPanel: NSOpenPanel = .init()
-                            openPanel.title = L10n.Panel.Open.title
-                            openPanel.allowsMultipleSelection = true
-                            openPanel.canChooseDirectories = true
-                            openPanel.canChooseFiles = true
-                            openPanel.begin { response in
-                                guard response == .OK else { return }
-
-                                store.send(.searchManifests(filePaths: openPanel.urls))
-                            }
-                        },
-                        fetchLicenses: { store.send(.fetchLicenses) },
-                        exportLicenses: {
-                            let savePanel: NSSavePanel = .init()
-                            savePanel.title = L10n.Panel.Save.title
-                            savePanel.canCreateDirectories = true
-                            savePanel.showsTagField = false
-                            savePanel.nameFieldStringValue = L10n.Panel.Save.filename
-                            savePanel.level = .modalPanel
-                            savePanel.begin { result in
-                                guard result == .OK, let destination: URL = savePanel.url else { return }
-
-                                store.send(.exportLicenses(destination: destination))
-                            }
-                        }
-                    )
+            VStack {
+                NavigationView {
+                    MasterView()
+                    DetailView()
                 }
+                .listStyle(SidebarListStyle())
+            }
+            .toolbar {
+                ToolbarItems(
+                    areButtonsEnabled: !store.state.isProcessing,
+                    openFiles: {
+                        let openPanel: NSOpenPanel = .init()
+                        openPanel.title = L10n.Panel.Open.title
+                        openPanel.allowsMultipleSelection = true
+                        openPanel.canChooseDirectories = true
+                        openPanel.canChooseFiles = true
+                        openPanel.begin { response in
+                            guard response == .OK else { return }
+
+                            store.send(.searchManifests(filePaths: openPanel.urls))
+                        }
+                    },
+                    fetchLicenses: { store.send(.fetchLicenses) },
+                    exportLicenses: {
+                        let savePanel: NSSavePanel = .init()
+                        savePanel.title = L10n.Panel.Save.title
+                        savePanel.canCreateDirectories = true
+                        savePanel.showsTagField = false
+                        savePanel.nameFieldStringValue = L10n.Panel.Save.filename
+                        savePanel.level = .modalPanel
+                        savePanel.begin { result in
+                            guard result == .OK, let destination: URL = savePanel.url else { return }
+
+                            store.send(.exportLicenses(destination: destination))
+                        }
+                    }
+                )
+            }
         }
         .onReceive(NotificationCenter.default.publisher(for: .errorMessageChanged)) { notification in
             guard let errorMessage: String = notification.userInfo?[String.errorMessageKey] as? String else { return }
