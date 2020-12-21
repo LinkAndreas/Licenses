2c79dfc6df8810b9c33f08109dec645d1fa3e6b1
Andreas Link <mail@linkandreas.de>
2020-12-21 07:13:20 +0100

    [Release] Bump build number

--- App/Sources/Globals/Extensions/NSToolbarItemExtensions.swift
+++
@@ -1,8 +0,0 @@
-//  Copyright © 2020 Andreas Link. All rights reserved.
-
-import Cocoa
-
-extension NSToolbarItem.Identifier {
-    static let fetchLicenses: Self = .init("de.linkandreas.licenses.toolbar_item.fetchLicenses")
-    static let exportLicenses: Self = .init("de.linkandreas.licenses.toolbar_item.exportLicenses")
-}
