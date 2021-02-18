b3d0b257d27c0a83d62f436c6042149d01f3f6fb
Andreas Link <mail@linkandreas.de>
2021-02-18 11:19:58 +0100

    Include reducer dependencies in the environment instead of referencing them directly

--- App/Sources/Architecture/AppEnvironment.swift
+++ App/Sources/Architecture/AppEnvironment.swift
@@ -1,3 +1,23 @@
 //  Copyright © 2021 Andreas Link. All rights reserved.
 
-struct AppEnvironment { /* Empty*/ }
+struct AppEnvironment {
+    let settingsDataSource: SettingsSynchronousDataSource
+    let cocoaPodsProcessor: RepositoryProcessor
+    let licenseProcessor: RepositoryProcessor
+    let csvRowFactory: CSVRowFactory
+    let csvExporter: CSVExporter
+
+    init(
+        settingsDataSource: SettingsSynchronousDataSource = Defaults.shared,
+        cocoaPodsProcessor: RepositoryProcessor = CocoaPodsRepositoryProcessor(),
+        licenseProcessor: RepositoryProcessor = LicenseRepositoryProcessor(),
+        csvRowFactory: CSVRowFactory = .init(),
+        csvExporter: CSVExporter = DefaultCSVExporter()
+    ) {
+        self.settingsDataSource = settingsDataSource
+        self.cocoaPodsProcessor = cocoaPodsProcessor
+        self.licenseProcessor = licenseProcessor
+        self.csvRowFactory = csvRowFactory
+        self.csvExporter = csvExporter
+    }
+}
--- App/Sources/Globals/UserDefaults/Defaults.swift
+++ App/Sources/Globals/UserDefaults/Defaults.swift
@@ -1,7 +1,9 @@
 //  Copyright © 2021 Andreas Link. All rights reserved.
 
-enum Defaults {
-    @Storage(key: "token", defaultValue: "") static var token: String
-    @Storage(key: "isAutomaticFetchEnabled", defaultValue: true) static var isAutomaticFetchEnabled: Bool
-    @Storage(key: "isOnboardingCompleted", defaultValue: false) static var isOnboardingCompleted: Bool
+struct Defaults: SettingsSynchronousDataSource {
+    static let shared: Self = .init()
+
+    @Storage(key: "token", defaultValue: "") var token: String
+    @Storage(key: "isAutomaticFetchEnabled", defaultValue: true) var isAutomaticFetchEnabled: Bool
+    @Storage(key: "isOnboardingCompleted", defaultValue: false) var isOnboardingCompleted: Bool
 }
